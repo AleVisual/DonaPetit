b745755fed1091223b7d4b819754e11c68110242
AleVisual <pc_ale44@outlook.com>
2026-05-19 22:03:07 -0300

    Fix spelling and update badge for a product

--- js/data/products.js
+++ js/data/products.js
@@ -202,10 +202,10 @@ const PRODUCTS = {
     },
     {
       id: 'choco-13',
-      name: 'Bolsita de BomBones Surtidos',
+      name: 'Bolsita de Bombones Surtidos',
       desc: 'Dulce de leche, Coffler, Menta, Nutella, Bananita y mucho más.',
       img: 'ProductoChocolates/BolsitaBomBonesSurtido.jpg',
-      badge: '❤️ Favorito',
+      badge: '✨ Nuevo',
     },
   ],
 };
